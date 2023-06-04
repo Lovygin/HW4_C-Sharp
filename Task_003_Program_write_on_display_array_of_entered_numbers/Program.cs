12db5a862d8dedd943531b061b7dfa38ca9810e9
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-06-04 16:19:08 +0300

    Problem solved for long numbers, you can store more than 10 values in an array

--- Task_003_Program_write_on_display_array_of_entered_numbers/Program.cs
+++ Task_003_Program_write_on_display_array_of_entered_numbers/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-Console.Clear();
+/*Console.Clear();
 Console.WriteLine("Please, enter integer values");
 string x = Console.ReadLine();
 string[] numbersStringArr = x.Split(' ');
@@ -12,26 +12,20 @@ System.Console.WriteLine("Array: ");
 foreach (int number in digits)//перебираем массив и выводим отдельно каждый элемент
 {
     Console.Write(number);
+}*///Данное решение огрпничено 10-ю знаками в массиве (расширение int для массива)
+
+
+Console.Clear();
+Console.WriteLine("Please, enter integer values");
+string x = Console.ReadLine();
+string[] numbersStringArr = x.Split(' ');
+long[] digits = new long[numbersStringArr.Length];
+for(int i = 0; i < numbersStringArr.Length; i++)
+{
+    digits[i] = long.Parse(numbersStringArr[i]);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//for(int i = 0; i < numbersString.Length; i++ )
-//{
-//    bool success = int.TryParse(numbersString[i], out digits[i]);
-//    if(success)
-//    {
-//
-//    }
-//}
+System.Console.WriteLine("Array: ");
+foreach (long number in digits)//перебираем массив и выводим отдельно каждый элемент
+{
+    Console.Write(number);
+}
